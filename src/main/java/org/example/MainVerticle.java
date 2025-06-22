5145185faa172c7c47d034083aa1a3e91d2a05b5
Basu-D <129307410+basu-d@users.noreply.github.com>
2025-06-22 11:42:44 +0530

    Initial code setup

---
+++ src/main/java/org/example/MainVerticle.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.Router;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() {
+        Router router = Router.router(vertx);
+        router.route("/health").handler(context -> context.response().end("UP"));
+
+        vertx.createHttpServer()
+                .requestHandler(router)
+                .listen(8888, http -> {
+                    if(http.succeeded()) {
+                        System.out.println("HTTP Server started on port 8888");
+                    } else {
+                        System.out.println("HTTP Server failed to start");
+                    }
+                });
+    }
+
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(new MainVerticle());
+
+    }
+}
